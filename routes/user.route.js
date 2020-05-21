90e8291807dbe0da28d045bc6b1d49ca1896688d
KhacTrieu <khtrieu1102@gmail.com>
2020-05-22 01:46:35 +0700

    Prettier code, update users balance.

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,167 +1,157 @@
-const express = require('express');
-const moment = require('moment');
-const md5 = require('md5');
-const NodeRSA = require('node-rsa');
-const usersModel = require('../models/users.model');
-const process = require('../config/process.config')
+const express = require("express");
+const moment = require("moment");
+const md5 = require("md5");
+const NodeRSA = require("node-rsa");
+const usersModel = require("../models/users.model");
+const process = require("../config/process.config");
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-  const user = new usersModel(req.body);
-  user.setPasswordHash(req.body.password);
-  user
-    .save()
-    .then(userData => {
-      res.status(200).send({ user: userData });
-    })
-    .catch(err => {
-      console.log('error: ', err.message);
-      return res
-        .status(500)
-        .send({ message: 'Đã có lỗi xảy ra, vui lòng thử lại' });
-    });
-})
+const confirm = (req) => {
+	const ts = req.get("ts");
+	const partnerCode = req.get("partnerCode");
+	const hashedSign = req.get("hashedSign");
 
-const confirm = (req) =>{
-  const ts = req.get('ts');
-  const partnerCode = req.get('partnerCode');
-  const hashedSign = req.get('hashedSign');
+	const comparingSign = md5(ts + req.body + md5("dungnoiaihet"));
+	console.log(`${comparingSign} - ${hashedSign}`);
+	if (ts <= moment().unix() - 150) {
+		return 1;
+	}
 
-  const comparingSign = md5(ts + req.body + md5("dungnoiaihet"))
-  console.log(comparingSign);
-  if (ts <= moment().unix() - 150) {
-    return 1;
-  }
+	// console.log(partnerCode)
+	if (partnerCode != "huuTien123") {
+		return 2;
+	}
 
-  // console.log(partnerCode)
-  if (partnerCode != "huuTien123") {
-    return 2;
-  }
+	if (hashedSign != comparingSign) {
+		return 3;
+	}
 
-  if(hashedSign != comparingSign){
-    return 3;
-  }
+	if (!req.body.id) {
+		return 4;
+	} else {
+		return 0;
+	}
+};
 
-  if (!req.body.id) {
-    return 4;
-  }
-  else
-  {
-    return 0;
-  }
+// ----- Register new user -----
+router.post("/", async (req, res) => {
+	const user = new usersModel(req.body);
+	user.setPasswordHash(req.body.password);
+	user
+		.save()
+		.then((userData) => {
+			res.status(200).send({ user: userData });
+		})
+		.catch((err) => {
+			console.log("error: ", err.message);
+			return res
+				.status(500)
+				.send({ message: "Đã có lỗi xảy ra, vui lòng thử lại" });
+		});
+});
 
-}
+// ----- Get all users in database ----- INTERNAL
+router.get("/", async (req, res) => {
+	const allUsers = await usersModel
+		.find()
+		.then((result) => result)
+		.catch((err) => {
+			throw new Error(err);
+		});
 
-router.get('/customer/', async (req, res) => {
+	if (allUsers.length > 0) {
+		return res.send(allUsers);
+	}
+	return res.json({
+		error: "Không có dữ liệu nào của người dùng!",
+	});
+});
 
-  // // console.log(moment().unix());
+// --EXTERNAL--- Post to get specific customer (with verify method) ---
+router.post("/customer/", async (req, res) => {
+	var con = confirm(req);
+	switch (con) {
+		case 1:
+			return res.status(400).send({
+				message: "Thời gian request quá hạn",
+			});
+		case 2:
+			return res.status(400).send({
+				message: "Bạn không là đối tác",
+			});
+		case 3:
+			return res.status(400).send({
+				message: "Tệp tin có thể đã bị sửa đổi",
+			});
+		case 4:
+			return res.status(400).send({
+				message: "Không nhận được ID",
+			});
+	}
 
-  var con = confirm(req);
-  // console.log(con);
-  if (con == 1) {
-    return res.status(400).send({
-      message: 'Thời gian request quá hạn'
-    });
-  }
+	await usersModel.findOne({ _id: req.body.id }, (err, data) => {
+		if (err) {
+			return res
+				.status(500)
+				.send({ message: "Đã có lỗi xảy ra, vui lòng thử lại!" });
+		}
+		if (data) {
+			// Chỉ trả về tên của người dùng thôi.
+			return res.status(200).send({ user: data.name.toUpperCase() });
+		} else {
+			return res.status(403).send({ message: "Không có dữ liệu" });
+		}
+	});
+});
 
-  if (con == 2) {
-    return res.status(400).send({
-      message: 'Bạn không là đối tác'
-    });
-  }
+//
+router.post("/transaction/", async (req, res) => {
+	const sign = req.get("sign");
+	const keyPublic = new NodeRSA(process.huuTien.RSA_PUBLICKEY);
 
-  if(con == 3){
-    return res.status(400).send({
-      message: 'Tệp tin có thể đã bị sửa đổi'
-    });
-  }
-
-  if (con == 4) {
-    return res.status(400).send({
-      message: 'Không nhận được ID'
-    });
-  }
-
-  await usersModel.findOne({ _id: req.body.id }, (err, data) => {
-    if (err) {
-      return res
-        .status(500)
-        .send({ message: 'Đã có lỗi xảy ra, vui lòng thử lại!' });
-    }
-    if (data) {
-      return res
-        .status(200)
-        .send({ user: data });
-    }
-    else{
-      return res
-      .status(403)
-      .send({ message: "Không có dữ liệu" });
-    }
-
-  });
-})
-
-router.get('/transaction/', async (req, res) => {
-
-  const sign = req.get('sign');
-  const keyPublic = new NodeRSA(process.huuTien.RSA_PUBLICKEY);
-  var veri = keyPublic.verify(req,sign,"base64","base64")
-
-  var con = confirm(req);
-  // console.log(con);
-  if (con == 1) {
-    return res.status(400).send({
-      message: 'Thời gian request quá hạn'
-    });
-  }
-
-  if (con == 2) {
-    return res.status(400).send({
-      message: 'Bạn không là đối tác'
-    });
-  }
-
-  if(con == 3){
-    return res.status(400).send({
-      message: 'Tệp tin có thể đã bị sửa đổi'
-    });
-  }
-
-  if (con == 4) {
-    return res.status(400).send({
-      message: 'Không nhận được ID'
-    });
-  }
-
-  if(veri != true){
-    return res.status(400).send({
-      message: 'Sai chữ kí'
-    });
-  }
-
-  await usersModel.findOne({ _id: req.body.id }, (err, data) => {
-    if (err) {
-      return res
-        .status(500)
-        .send({ message: 'Đã có lỗi xảy ra, vui lòng thử lại!' });
-    }
-    if (data) {
-      return res
-        .status(400)
-        .send({ user: data });
-    }
-    else{
-      return res
-      .status(403)
-      .send({ message: "Không có dữ liệu" });
-    }
-
-
-  });
-})
-
-
+	// bodyjson: {id: _id, amount: 50000}
+	var veri = keyPublic.verify(req.body, sign, "base64", "base64");
+	var con = confirm(req);
+	// console.log(con);
+	switch (con) {
+		case 1:
+			return res.status(400).send({
+				message: "Thời gian request quá hạn",
+			});
+		case 2:
+			return res.status(400).send({
+				message: "Bạn không là đối tác",
+			});
+		case 3:
+			return res.status(400).send({
+				message: "Tệp tin có thể đã bị sửa đổi",
+			});
+		case 4:
+			return res.status(400).send({
+				message: "Không nhận được ID",
+			});
+	}
+	if (veri != true) {
+		return res.status(400).send({
+			message: "Sai chữ kí",
+		});
+	}
+	await usersModel.findOne({ _id: req.body.id }, async (err, data) => {
+		if (err) {
+			return res
+				.status(500)
+				.send({ message: "Đã có lỗi xảy ra, vui lòng thử lại!" });
+		}
+		if (data) {
+			// ? tại sao trả về 400?
+			data.balance += req.body.amount;
+			await data.save();
+			return res.json({ username: data.name, balance: data.balance });
+		} else {
+			return res.status(403).send({ message: "Không có dữ liệu" });
+		}
+	});
+});
 
 module.exports = router;
