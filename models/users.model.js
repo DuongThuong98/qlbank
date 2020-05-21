930c29c3e56f4dd982f00f4c2c3d806cb0185231
KhacTrieu <khtrieu1102@gmail.com>
2020-05-22 01:45:20 +0700

    Add [balance, phone, userId] fields for users collection.

--- models/users.model.js
+++ models/users.model.js
@@ -1,38 +1,38 @@
-
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const saltRounds = 10;
 
-const UserSchema = mongoose.Schema({
-  // entity = {
-      // "username": "admin",
-      // "password_hash": "admin",
-      // "name": "admin",
-      // "email": "admin@g.c",
-      // "dob": "1990-09-09",
-      // "permission": 0
-    // }
-  passwordHash: String,
-  avatar: String,
-  phone: String,
-  username: String,
-  passwordHash: String,
-  name: String,
-  email: String,
-  dob: Date,
-  permission: Boolean,
-  refreshToken: String,
-  rdt: Date
-   
-}, {
-    timestamps: true,
-});
+const UserSchema = mongoose.Schema(
+	// {
+	// 	"username": "trieu",
+	// 	"password": "1234",
+	// 	"name": "Nguyễn Ngọc Khắc Triệu",
+	// 	"email": "khactrieu@gmail.com",
+	// 	"phone": "0903020192",
+	// 	dob: Date,
+	// 	"avatar": "12345",
+	// }
+	{
+		userId: String, // Tai khoan ngan hang, chỉ số dài 7 kí tự. Vd: "4224201"
+		username: String,
+		passwordHash: String,
+		name: String, // Tên đầy đủ
+		email: String,
+		phone: String,
+		balance: { type: Number, default: 50000 },
+		permission: { type: Boolean, default: true },
+		refreshToken: String,
+		rdt: Date,
+	},
+	{
+		timestamps: true,
+	}
+);
 
-UserSchema.methods.setPasswordHash = function(password) {
-  this.passwordHash = bcrypt.hashSync(password, saltRounds);
+UserSchema.methods.setPasswordHash = function (password) {
+	this.passwordHash = bcrypt.hashSync(password, saltRounds);
 };
-
 
 const Users = mongoose.model("Users", UserSchema);
 module.exports = Users;
