16f2d11edeb8eef7bf3e829217df444d838c991c
KhacTrieu <khtrieu1102@gmail.com>
2020-05-22 01:43:17 +0700

    Use local mongodb to test

--- app.js
+++ app.js
@@ -1,62 +1,55 @@
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
-require('express-async-errors');
+require("express-async-errors");
 
-const config = require('./config/default.json');
-const verify = require('./middlewares/auth.mdw');
+const config = require("./config/default.json");
+const verify = require("./middlewares/auth.mdw");
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
 //connecting to the database
 mongoose.Promise = global.Promise;
 mongoose
-    .connect(config.monggodb.url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-    })
-    .then(() => {
-        console.log("Successfully connected to the database");
-    })
-    .catch((err) => {
-        console.log("Could not connected to the database. Exiting now...", err);
-        process.exit();
-    });
+	.connect(config.mongodb.localUrl, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	})
+	.then(() => {
+		console.log("Successfully connected to the database");
+	})
+	.catch((err) => {
+		console.log("Could not connected to the database. Exiting now...", err);
+		process.exit();
+	});
 
-app.get('/', (req, res) => {
-  res.json({
-    msg: 'hello from nodejs express api'
-  });
-})
+app.get("/", (req, res) => {
+	res.json({
+		msg: "hello from nodejs express api",
+	});
+});
 
-// app.use('/api/auth', require('./routes/auth.route'));
-app.use('/api/users', require('./routes/user.route'));
-// app.use('/api/categories', verify, require('./routes/category.route'));
-// app.use('/api/products', verify, require('./routes/product.route'));
+app.use("/api/users", require("./routes/user.route"));
+app.use("/api/banks", require("./routes/bank.route"));
 
 app.use((req, res, next) => {
-  res.status(404).send('NOT FOUND');
-})
+	res.status(404).send("NOT FOUND");
+});
 
 app.use(function (err, req, res, next) {
-  console.log(err.stack);
-  // console.log(err.status);
-  const statusCode = err.status || 500;
-  res.status(statusCode).send('View error log on console.');
-})
+	console.log(err.stack);
+	// console.log(err.status);
+	const statusCode = err.status || 500;
+	res.status(statusCode).send("View error log on console.");
+});
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`API running on port ${PORT}`)
-})
-// const PORT = 3000;
-// app.listen(PORT, _ => {
-//   console.log(`API is running at http://localhost:${PORT}`);
-// })
-
+	console.log(`API running on port ${PORT}`);
+});
