81efed6a126e15910e534c0e2f3c1a3a69ddf165
KhacTrieu <khtrieu1102@gmail.com>
2020-05-25 20:54:45 +0700

    Add client method to another bank server.

--- routes/external.route.js
+++ routes/external.route.js
@@ -4,6 +4,7 @@ const md5 = require("md5");
 const NodeRSA = require("node-rsa");
 const usersModel = require("../models/users.model");
 const process = require("../config/process.config");
+const axios = require("axios");
 
 const router = express.Router();
 
@@ -17,8 +18,7 @@ const confirm = (req) => {
 	if (ts <= moment().unix() - 150) {
 		return 1;
 	}
-	
-	
+
 	if (partnerCode != "3TBank" && partnerCode != "baoSon123") {
 		return 2;
 	}
@@ -27,14 +27,14 @@ const confirm = (req) => {
 		return 3;
 	}
 
-	if (!req.body.id) {
+	if (!req.body.accountNumber) {
 		return 4;
 	} else {
 		return 0;
 	}
 };
 
-// --EXTERNAL--- Post to get specific customer (with verify method) ---
+// --EXTERNAL-SERVER--- nhận request, hash và trả về tên người dùng cho ngân hàng khác ---
 router.post("/customer/", async (req, res) => {
 	// req -> headers [ts, partnerCode, hashedSign]
 	// req -> body {id: 1}
@@ -59,31 +59,32 @@ router.post("/customer/", async (req, res) => {
 			});
 	}
 
-	await usersModel.findOne({ accountNumber: req.body.id }, (err, data) => {
-		if (err) {
-			return res
-				.status(500)
-				.send({ message: "Đã có lỗi xảy ra, vui lòng thử lại!" });
+	await usersModel.findOne(
+		{ accountNumber: req.body.accountNumber },
+		(err, data) => {
+			if (err) {
+				return res
+					.status(500)
+					.send({ message: "Đã có lỗi xảy ra, vui lòng thử lại!" });
+			}
+			if (data) {
+				// Chỉ trả về tên của người dùng thôi.
+				return res.status(200).send({ user: data.name.toUpperCase() });
+			} else {
+				return res.status(403).send({ message: "Không có dữ liệu" });
+			}
 		}
-		if (data) {
-			// Chỉ trả về tên của người dùng thôi.
-			return res.status(200).send({ user: data.name.toUpperCase() });
-		} else {
-			return res.status(403).send({ message: "Không có dữ liệu" });
-		}
-	});
+	);
 });
 
-//
+// --EXTERNAL-SERVER--- nhận request, hash + verify và nạp tiền
 router.post("/transaction/", async (req, res) => {
 	// req -> headers [ts, partnerCode, hashedSign] + [sign (req.body-RSA)]
-	// req -> bodyjson: {sentId: _id, bankId: 1, receivedId: _id, amount: 50000, content: "Tien an 2020", [timestamps]}
+	// req -> bodyjson: {sentId: _id, bankId: 1, accountNumber: _id, amount: 50000, content: "Tien an 2020", [timestamps]}
 	// response -> "thành công hay không"
-	const sign = req.get("sign");
-	const keyPublic = new NodeRSA(process.huuTien.RSA_PUBLICKEY);
 
-	// bodyjson: {sentId: _id, bankId: 1, receivedId: _id, amount: 50000, content: "Tien an 2020", [timestamps]}
-	var veri = keyPublic.verify(req.body, sign, "base64", "base64");
+	// bodyjson: {sentId: _id, bankId: 1, accountNumber: _id, amount: 50000, content: "Tien an 2020", [timestamps]}
+
 	var con = confirm(req);
 
 	switch (con) {
@@ -104,13 +105,31 @@ router.post("/transaction/", async (req, res) => {
 				message: "Không nhận được ID",
 			});
 	}
+
+	// const sign = req.get("sign");
+	let Key_Public;
+	switch (req.get("partnerCode")) {
+		case "baoSon123":
+			Key_Public = process.huuTien.RSA_PUBLICKEY;
+			break;
+		case "3TBank":
+			Key_Public = process.huuTien.RSA_PUBLICKEY;
+			break;
+		default:
+			Key_Public = 0;
+	}
+
+	const sign = req.get("sign");
+	const keyPublic = new NodeRSA(Key_Public);
+	var veri = keyPublic.verify(req.body, sign, "base64", "base64");
+
 	if (veri != true) {
 		return res.status(400).send({
 			message: "Sai chữ kí",
 		});
 	}
 	await usersModel.findOne(
-		{ accountNumber: req.body.receivedId },
+		{ accountNumber: req.body.accountNumber },
 		async (err, user) => {
 			if (err) {
 				return res
@@ -136,6 +155,64 @@ router.post("/transaction/", async (req, res) => {
 			}
 		}
 	);
+});
+
+const SERVER_URL = "https://qlbank1.herokuapp.com";
+
+router.post("/3TBank/customer", async (req, res) => {
+	const timeStamp = Date.now();
+	const partnerCode = "3TBank"; // SAPHASANBank
+	const bodyJson = { id: "12345" };
+	const signature = timeStamp + bodyJson + md5("dungnoiaihet");
+
+	await axios
+		.post(`${SERVER_URL}/api/external/customer`, bodyJson, {
+			headers: {
+				ts: timeStamp,
+				partnerCode: partnerCode,
+				hashedSign: md5(signature),
+			},
+		})
+		.then((result) => {
+			const { data } = result;
+			if (result.data) res.json(result.data);
+		})
+		.catch((error) => {
+			res.status(500).json(error);
+		});
+});
+
+router.get("/3TBank/transaction", async (req, res) => {
+	const timeStamp = Date.now();
+	const partnerCode = "3TBank";
+	const bodyJson = {
+		accountNumber: req.body.accountNumber,
+		amount: req.body.amount,
+	};
+	const signature = timeStamp + bodyJson + md5("dungnoiaihet");
+	const privateKey = new NodeRSA(
+		"-----BEGIN RSA PRIVATE KEY-----\n" +
+			"MIICXwIBAAKBgQCyceITLtFoy4KzMgmr6NEnvk1VBH7pRuyyg7IkXc3kBspKs9CIErm2eJtEtduIPQK+3AgiQW+fjL1dDMQr7ENZiGzWhEPoSbU348mjg1fxFDztFB4QiqAd7UUvj1kK2/UT+D0C6Sgc0O69C9lRGahPSAX+7ZArGIodtfuOKPenEwIDAQABAoGBAKU98CvzXte8HPvziiE3Jve2scXYs+0xUF6+tWgXtWFDKHCksqZPMMpYRPALt48hcDltZ9rQ3ZzRp0lTWRWTY4kmnjUm1W4E7uFmJJc7KySZJH9XNbvlOceVIKPIWjZvvQ93wov03G2ajdv/NC2BT57xQ+YTaMe3GQkJGTX7V/KBAkEA8TQmBdaExOBF7mrGKMrrrvYnErtZWN4dLdPK+ipfmeSM/oD25/UHfPHbh8tkHbt9vfz4PF/3NdAWcZiMNzAKPwJBAL1kLC/SM9NFfxCLfQrmP1qTASWs4IVsxeYU4+dUVcUwL0g4WlUgCjrVCFYomWen1wCbqCvlGpON9H7CLR7fpi0CQQDI3cXAXNoqXh6+orqtI/fLt3/okI6ifC5OiK7jUEBXF0b3dwynNJ3sxjksyAty2z2m5zEOjlh/vu/B3+j82IvfAkEAqlR2PQgCnicpkPqymePb5JzDclvZjYX3Medl1L4PaYndbElqTJbFPIYtujdHSGc1wZE8nUWuMjiARKRkKhkgfQJBAIqWxELwATG3541h/7MKI2tnTC0F3g7nTLJWtgIiqYfyw/jFdsVGWZUlJriyS6LxYh+0zMdRtdscw4iWEPJ2vM4=\n" +
+			"-----END RSA PRIVATE KEY-----"
+	);
+	const sign = privateKey.sign(bodyJson, "base64", "base64");
+	await axios
+		.post(`${SERVER_URL}/api/external/transaction`, bodyJson, {
+			headers: {
+				ts: timeStamp,
+				partnerCode: partnerCode,
+				hashedSign: md5(signature),
+				sign: sign,
+			},
+			// data: `id=${customerId}`,
+		})
+		.then((result) => {
+			const { data } = result;
+			res.json(data);
+		})
+		.catch((error) => {
+			res.json(error);
+		});
 });
 
 module.exports = router;
