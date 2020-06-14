b046abaa2eb9a3f30d839ed247c9f59618301c8e
KhacTrieu <khtrieu1102@gmail.com>
2020-06-15 02:28:29 +0700

    Move forgot password function to auth

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,6 +2,10 @@ const express = require("express");
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
 
+const { totp } = require("otplib");
+const nodemailer = require("nodemailer");
+const KeyGenCode = "key-gen-otp-code";
+
 const router = express.Router();
 
 // --- Login ---
@@ -27,5 +31,91 @@ const generateAccessToken = (username, role) =>
 		"secretKey",
 		{ expiresIn: "10m" }
 	);
+
+// region forgot password
+router.post("/forgot-password", async (req, res) => {
+	const email = req.body.email;
+
+	usersModel
+		.findOne({
+			email: email,
+		})
+		.exec((err, user) => {
+			if (err) {
+				res.status(500).send({ message: err });
+				return;
+			}
+
+			if (!user) {
+				return res.status(404).send({ message: "User Not found." });
+			}
+
+			totp.options = { step: 300 };
+			const code = totp.generate(KeyGenCode);
+			var transporter = nodemailer.createTransport({
+				host: "smtp.gmail.com",
+				port: 465,
+				secure: true,
+				auth: {
+					user: "mail to send ",
+					pass: "pass",
+				},
+				tls: {
+					rejectUnauthorized: false,
+				},
+			});
+
+			var content = "";
+			content += `<div>
+        <h2>Use the code below to reset password!</h2>
+				<h1> ${code}</h1>
+				<p>This code will be expired after 5 minutes!</p>
+				</div>  
+		`;
+
+			var mailOptions = {
+				from: `huuthoigialai@gmail.com`,
+				to: email,
+				subject: "Gửi Mã OTP",
+				html: content,
+			};
+
+			transporter.sendMail(mailOptions, function (error, info) {
+				if (error) {
+					console.log(error);
+					return res.status(400).json({ succes: false });
+				} else {
+					console.log("Email sent: " + info.response);
+					return res.json({ succes: true });
+				}
+			});
+		});
+});
+
+router.post("/verify-forgot-password", async (req, res) => {
+	const { code, newPassword, email } = req.body;
+	usersModel.findOne({ email: email }).exec((err, user) => {
+		if (err) return res.status(500).send({ message: err });
+		if (!user) return res.status(404).send({ message: "User Not found." });
+	});
+
+	const isValid = totp.check(code, KeyGenCode);
+	if (isValid) {
+		newPasswordHash = bcrypt.hashSync(newPassword, 10);
+		const result = await usersModel.findOneAndUpdate(
+			{ email },
+			{ passwordHash: newPasswordHash }
+		);
+		if (result) {
+			res.json({ succes: true, message: "Reset password success" });
+		} else {
+			res.status(401).json({ message: "Authentication error!" });
+		}
+	} else {
+		res.status(401).json({ message: "Code is invalid or expried!" });
+	}
+});
+
+//end region forgot password
 
 module.exports = router;
