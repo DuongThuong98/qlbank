5d2197b560fac5af2a21c0d6450c23e919413cc2
HuuThoi <thoi.huynh@nois.com>
2020-06-27 14:40:56 +0700

    admin deposit

--- models/transaction.model.js
+++ models/transaction.model.js
@@ -1,41 +1,47 @@
 const mongoose = require("mongoose");
 
 const TransactionSchema = mongoose.Schema(
-	{
-		sentUserId: {
-			type: String,
-			required: true,
-		},
-		sentBankId: {
-			type: Number,
-			required: true,
-		},
-		receivedUserId: {
-			type: String,
-			required: true,
-		},
-		receivedBankId: {
-			type: Number,
-			required: true,
-		},
-		isDebt: Boolean, // Có phải trả nợ không?
-		isReceiverPaid: Boolean, // Người nhận trả phí giao dịch? => True: người nhận trả, false: người gửi trả.
-		amount: {
-			type: Number,
-			required: true,
-		},
-		content: String,
-		signature: String,
-		isVerified: {
-			type: Boolean,
-			default: false,
-		},
-		transactionIdCode: String,
-		code: String,
-	},
-	{
-		timestamps: true,
-	}
+  {
+    sentUserId: {
+      type: String,
+      required: true,
+    },
+    sentBankId: {
+      type: Number,
+      required: true,
+    },
+    receivedUserId: {
+      type: String,
+      required: true,
+    },
+    receivedBankId: {
+      type: Number,
+      required: true,
+    },
+    isDebt: {
+      type: Boolean,
+      default: false,
+    },
+    isReceiverPaid: {
+      type: Boolean, // Người nhận trả phí giao dịch? => True: người nhận trả, false: người gửi trả.
+      default: false,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    content: String,
+    signature: String,
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    transactionIdCode: String,
+    code: String,
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const Transaction = mongoose.model("Transaction", TransactionSchema);
---
+++ routes/admin.route.js
@@ -0,0 +1,115 @@
+const express = require("express");
+const router = express.Router();
+const { totp } = require("otplib");
+const nodemailer = require("nodemailer");
+const md5 = require("md5");
+const config = require("../config/default.json");
+
+const TransactionModel = require("../models/transaction.model");
+const UserModel = require("../models/users.model");
+const { message } = require("openpgp");
+
+const keyOTP = "#@vevyveryOTPsecretkey@#";
+const fees = 10000;
+const minimumAmount = 50000;
+
+// router.get("/all-transaction", async (req, res) => {
+//   const allUserTrans = await TransactionModel.find()
+//     .then((data) => data)
+//     .catch((err) => {
+//       throw new Error(err);
+//     });
+
+//   allUserTrans.length !== 0
+//     ? res.json(allUserReceiver)
+//     : res.json({ message: "Không giao dịch nào" });
+// });
+
+// Thêm một Giao dịch
+//input: model 	//	"sentUserId": "Number",
+// 	"sentBankId": "Number",
+// 	"receivedUserId": "Number",
+// 	"receivedBankId": "Number",
+// 	"isDebt": false, // Có phải trả nợ không?
+// 	"isReceiverPaid": true, // Người nhận trả phí giao dịch? => True: người nhận trả, false: người gửi trả.
+// 	"amount": 10000,
+//	isVerified: Boolean
+// 	"content": "Thông tin trả nợ",
+// 	"signature": "Chữ kỹ ở này",
+// }
+
+//current user is ADMIN
+router.post("/deposit", async (req, res) => {
+  const { receivedUserId, receivedBankId, amount, content } = req.body;
+  const currentUser = req.user;
+
+  //model
+  const model = {
+    sentUserId: currentUser.id,
+    sentBankId: currentUser.bankId ? currentUser.bankId : 0,
+    receivedUserId: receivedUserId,
+    receivedBankId: receivedBankId,
+    isDebt: false,
+    isVerified: false,
+    isReceiverPaid: false,
+    amount: amount,
+    content: content,
+    signature: (bankId = 0 ? "" : signature),
+  };
+
+  //validate
+  if (amount < minimumAmount)
+    return res.status(400).json({
+      message: "Amount is invalid. Please over " + minimumAmount,
+    });
+
+  var receiverUser = await UserModel.findOne({ accountNumber: receivedUserId });
+  if (receiverUser == null)
+    return res.status(404).json({ message: "Receiver not found" });
+
+  //create
+  TransactionModel.create(model, async (err, tran) => {
+    if (err) {
+      return res.status(500).json({ message: err });
+    } else {
+      //add amount for receiverUser balance
+      receiverUser.balance = receiverUser.balance + amount;
+      await receiverUser.save();
+
+      //send mail
+      var transporter = nodemailer.createTransport(
+        config.emailTransportOptions
+      );
+
+      //send to receiver
+      var content = "";
+      content += `<div>
+					<h2>Bạn đã nạp ${amount} vào tài khoản.</h2>
+					<h1>Vào lúc: ${tran.createdAt} </h1>
+					<p>Your balance: ${receiverUser.balance}</p>
+					</div>`;
+
+      var mailOptions = {
+        from: `huuthoigialai@gmail.com`,
+        to: currentUser.email,
+        subject: "Thông báo nạp tiền vào tài khoản.",
+        html: content,
+      };
+
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+          return res.status(400).json({ success: false });
+        } else {
+          console.log("Email sent: " + info.response);
+          return res.json({ success: true });
+        }
+      });
+    }
+
+    tran.isVerified = true;
+    await tran.save();
+  });
+});
+
+module.exports = router;
