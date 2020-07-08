1aee1076a302129013f9aab6e2aa61a7bf0aa38d
HuuThoi <thoi.huynh@nois.com>
2020-07-08 12:15:14 +0700

    add notification when delete debt

--- models/notification.model.js
+++ models/notification.model.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 
 const NotificationSchema = mongoose.Schema(
-	{
-		notificationTitle: String,
-		notificationContent: String,
-		fromUserId: String, // ứng với AccountNumber, nếu bằng 0 là noti từ admin
-		fromBankId: Number,
-		toUserId: String, // ứng với AccountNumber, nếu bằng Null là gửi đến tất cả user trong nội bộ
-		toBankId: Number,
-		isSent: Boolean, // notification is send or not
-	},
-	{ timestamps: true }
+  {
+    notificationTitle: String,
+    notificationContent: String,
+    fromUserId: String, // ứng với AccountNumber
+    fromBankId: Number,
+    toUserId: String, // ứng với AccountNumber
+    toBankId: Number,
+    isSent: Boolean, // notification is send or not
+  },
+  { timestamps: true }
 );
 
 const Notification = mongoose.model("Notification", NotificationSchema);
